f3f8f178ab9e8abb3a71b9fa931569cfdf059b69
AkionGarro <carloscamp1008@estudianTEC.cr>
2022-10-06 13:06:18 -0600

    Update

--- DataBase.py
+++ DataBase.py
@@ -7,6 +7,7 @@ cnxn = pyodbc.connect('Driver={SQL Server};'
                       'TrustServerCertificate=no;'
                       'Connection Timeout=30;')
 cursor = cnxn.cursor()
-sql = "{call dbo.spINSERT_NewGame(1,'last of us',25,20,87,'35 horas','https://howlongtobeat.com/games/69695_Need_For_Speed_Heat.jpg?width=100')}"
-cursor.execute(sql)
+#sql = "{call dbo.spINSERT_NewGame(1,'last of us',25,20,87,'35 horas','https://howlongtobeat.com/games/69695_Need_For_Speed_Heat.jpg?width=100');}"
 
+cursor.execute("INSERT INTO Games (id,gameName,playPrice,amazonPrice,metaScore,howlong,imageLink) VALUES(5,'last of us',25,20,87,'35 horas','https://howlongtobeat.com/games/69695_Need_For_Speed_Heat.jpg?width=100');")
+cursor.commit()
